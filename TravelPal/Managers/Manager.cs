5f382bc3518786654e9144ea3b21f41a3d6385a0
MaxTarn <Maxtharn@gmail.com>
2023-10-23 23:17:18 +0200

    moved Manager class here

---
+++ TravelPal/Managers/Manager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPal.Pages;
+
+namespace TravelPal.Managers;
+
+internal static class Manager
+{
+    //---------- Pages ----------
+
+
+    internal static HeaderPage HeaderPage = new();
+    internal static LogInPage LogInPage = new();
+    internal static SplashScreenPage SplashScreenPage = new();
+
+
+    //---------- Pages END ----------
+
+
+
+    //---------- Managers ----------
+
+
+    internal static TravelManager TravelManager = new();
+    internal static UserManager UserManager = new();
+
+
+    //---------- Managers END ----------
+
+
+
+    //---------- The Main window ----------
+    internal static MainWindow MainWindow = new();
+}
+
