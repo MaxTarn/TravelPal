9e29f70d754b81d7fe586c9e821c84ed29e09f24
MaxTarn <Maxtharn@gmail.com>
2023-10-23 23:11:25 +0200

    added OtherItem class that implements IPackingListItem

---
+++ TravelPal/Classes/OtherItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPal.Interfaces;
+
+namespace TravelPal.Classes
+{
+    internal class OtherItem : IPackingListItem
+    {
+        public string Name { get; set; }
+        public string Info { get; set; }
+        public int Quantity { get; set; }
+
+        public OtherItem(string name, int quantity)
+        {
+
+        }
+    }
+}
