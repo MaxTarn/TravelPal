b2f61b67115cda92344c3455bde68bcfe684719c
MaxTarn <Maxtharn@gmail.com>
2023-10-23 23:10:38 +0200

    added IPackingListItem that is interface for the diffrent items that can be in the packing list

---
+++ TravelPal/Interfaces/IPackingListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPal.Interfaces
+{
+    internal interface IPackingListItem
+    {
+        public string Name { get; set; }
+        public string Info { get; set; }
+        //public string GetInfo();
+    }
+}
