407af1a5e02492d84af87cc3de1efe310ce6ab12
MaxTarn <Maxtharn@gmail.com>
2023-10-23 23:18:59 +0200

    added UserManager that will deal with login password validation, as well as other user related logic

---
+++ TravelPal/Managers/UserManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPal.Classes;
+using TravelPal.Interfaces;
+
+namespace TravelPal.Managers
+{
+    internal class UserManager
+    {
+        public List<IUser> Users = new();
+        public IUser SignedInUser { get; set; } = null;
+
+
+        public void AddUser(User addThisUser)
+        {
+
+        }
+
+        public UserManager()
+        {
+            Users.Add(new User());
+        }
+    }
+}
