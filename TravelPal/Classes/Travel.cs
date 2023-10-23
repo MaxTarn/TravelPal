8bbef87fe2f86a13e5f6741a203071468304013a
MaxTarn <Maxtharn@gmail.com>
2023-10-23 23:17:02 +0200

    added Travel class that is a description of a individual travel, with arrival country, start date, end date, and other relevant information (not tested)

---
+++ TravelPal/Classes/Travel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPal.Interfaces;
+
+namespace TravelPal.Classes;
+
+internal class Travel
+{
+    public string Destination { get; set; }
+    public AllCountries Country { get; set; }
+    public int Travellers { get; set; }
+    public List<IPackingListItem> PackingList;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int TravelDays { get; set; }
+
+    public Travel(string destination, AllCountries country, DateTime startDate, DateTime endDate)
+    {
+        this.Destination = destination;
+        this.Country = country;
+        this.StartDate = startDate;
+        this.EndDate = endDate;
+        TravelDays = endDate.Subtract(startDate).Days;
+
+        if (TravelDays <= 0)
+        {
+            //TODO implement error messege to the user
+            throw new NotImplementedException();
+        }
+    }
+}
+
