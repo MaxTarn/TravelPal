04b12291f1c5a13056b36105e0dddf6755a4b63e
MaxTarn <Maxtharn@gmail.com>
2023-10-23 23:09:15 +0200

    added User that Implemtns IUser

---
+++ TravelPal/Classes/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPal.Interfaces;
+
+namespace TravelPal.Classes
+{
+    internal class User : IUser
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public AllCountries Location { get; set; }
+        public List<Travel> Travels { get; set; } = new List<Travel>();
+
+        public User(string userName, string password, AllCountries location)
+        {
+            UserName = userName;
+            Password = password;
+            Location = location;
+        }
+    }
+}
