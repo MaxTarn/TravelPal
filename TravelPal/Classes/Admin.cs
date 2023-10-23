d8f4c4355273d9d47929d811f4b1109e17aec17d
MaxTarn <Maxtharn@gmail.com>
2023-10-23 23:08:59 +0200

    added Admin class that implements IUser

---
+++ TravelPal/Classes/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPal.Interfaces;
+
+namespace TravelPal.Classes
+{
+    internal class Admin : IUser
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public AllCountries Location { get; set; }
+    }
+}
