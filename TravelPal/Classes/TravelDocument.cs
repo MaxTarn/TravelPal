845e9ad2974c1a656e5ef65d89e8abcbbe557cd6
MaxTarn <Maxtharn@gmail.com>
2023-10-23 23:11:55 +0200

    added TravelDocument that implements IPackingListItem

---
+++ TravelPal/Classes/TravelDocument.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelPal.Interfaces;
+
+namespace TravelPal.Classes;
+
+internal class TravelDocument : IPackingListItem
+{
+    public string Name { get; set; }
+    public string Info { get; set; }
+    public bool Required { get; set; }
+
+
+    public TravelDocument(string name, bool required)
+    {
+
+    }
+}
+
