9dccae79c97789a39859ac605413e2280e3695b4
MaxTarn <Maxtharn@gmail.com>
2023-10-24 12:27:22 +0200

    added constructor

--- TravelPal/Classes/Admin.cs
+++ TravelPal/Classes/Admin.cs
@@ -12,5 +12,13 @@ namespace TravelPal.Classes
         public string UserName { get; set; }
         public string Password { get; set; }
         public AllCountries Location { get; set; }
+
+
+        public Admin(string username, string password, AllCountries location)
+        {
+            UserName = username;
+            Password = password;
+            Location = location;
+        }
     }
 }
