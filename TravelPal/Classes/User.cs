279a528bc6dab47fcdcdedfe963d0e1f57a3549c
MaxTarn <Maxtharn@gmail.com>
2023-10-24 23:26:10 +0200

    added constructor that takes in username, and password but no location. made class public

--- TravelPal/Classes/User.cs
+++ TravelPal/Classes/User.cs
@@ -7,18 +7,25 @@ using TravelPal.Interfaces;
 
 namespace TravelPal.Classes
 {
-    internal class User : IUser
+
+    public class User : IUser
     {
         public string UserName { get; set; }
         public string Password { get; set; }
         public AllCountries Location { get; set; }
-        public List<Travel> Travels { get; set; } = new List<Travel>();
+        public List<Travel> Travels { get; set; } = new();
 
         public User(string userName, string password, AllCountries location)
         {
             UserName = userName;
             Password = password;
             Location = location;
+        }
+
+        public User(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
         }
     }
 }
