9d3153a2778aa9fe8ce40b32843d3a6d9947d7dd
MaxTarn <Maxtharn@gmail.com>
2023-10-24 23:21:52 +0200

    moved the main manager class out from Managers folder. Found the massive error that made it impossible to change the frame.Content and anything else in the mainwindow at runtime.

---
+++ TravelPal/Manager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TravelPal;
+using TravelPal.Managers;
+using TravelPal.Pages;
+
+
+
+public class Manager
+{
+    //---------- Pages ----------
+
+
+    public static HeaderPage HeaderPage = new();
+    public static LogInPage LogInPage = new();
+    public static SplashScreenPage SplashScreenPage = new();
+
+
+    //---------- Pages END ----------
+
+
+
+    //---------- Managers ----------
+
+
+    public static TravelManager TravelManager = new();
+    public static UserManager UserManager = new();
+
+
+    //---------- Managers END ----------
+
+
+
+    //---------- The Main window ----------
+    public static MainWindow? Window = (MainWindow?)Application.Current?.MainWindow; // took me an entire day to debug and find that this worked
+
+
+}
+
