516875cf471cc0ac8f19df9332b3d26cf9392cc3
MaxTarn <Maxtharn@gmail.com>
2023-10-25 15:10:22 +0200

    added Country class, which either contains a EuCountry or a NonEuCountry, and some methods of accesing the enum(not tested)

---
+++ TravelPal/Classes/Country.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPal.Classes
+{
+    public class Country
+    {
+        private EUCountries? euCountry = null;
+        private NonEUcountries? nonEuCountry = null;
+
+        public bool IsEUCountry()
+        {
+            return nonEuCountry == null;
+        }
+
+        public bool IsNonEUCountry()
+        {
+            return euCountry == null;
+        }
+        public Country(EUCountries country)
+        {
+            euCountry = country;
+        }
+
+        public Country(NonEUcountries country)
+        {
+            nonEuCountry = country;
+        }
+
+        public void GetCountry(out EUCountries? country)
+        {
+            country = null;
+            if (IsNonEUCountry()) return;
+
+            country = (EUCountries)euCountry;
+        }
+        public void GetCountry(out NonEUcountries? country)
+        {
+            country = null;
+            if (IsEUCountry()) return;
+
+            country = (NonEUcountries)nonEuCountry;
+        }
+
+        public new string GetType()
+        {
+            if (IsEUCountry()) return "EU Country";
+            if (IsNonEUCountry()) return "Non EU country";
+            return "If you see this text then something is wrong with the TravelPal.Classes.Country Class";
+        }
+
+        public override string ToString()
+        {
+            if (IsEUCountry()) return euCountry.ToString();
+            if (IsNonEUCountry()) return euCountry.ToString();
+            return "If you see this text then something is wrong with the TravelPal.Classes.Country Class";
+        }
+    }
+}
