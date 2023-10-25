f6bfb82feb6c82b801996baeaabc3da4487d8c10
MaxTarn <Maxtharn@gmail.com>
2023-10-25 13:13:43 +0200

    added a summary for LogIn(), added null and empty checking for LogIn() parameters, now using guard clause i the foreach loop. LogIn() now returns a string with the error messege or string.empty when all is good

--- TravelPal/Managers/UserManager.cs
+++ TravelPal/Managers/UserManager.cs
@@ -20,21 +20,30 @@ public class UserManager
         Users.Add(addThisUser);
     }
 
-    public bool LogIn(string userName, string password)
+    /// <summary>
+    /// Loggs in the user with given userName and Password.
+    /// If the given username doesnt exist or the password is wrong
+    /// returns a non empty string with the error messege
+    /// When all is good returns string.empty
+    /// </summary>
+    /// <param name="userName"></param>
+    /// <param name="password"></param>
+    /// <returns>Returns string.empty when given username and password is good, else return is a error messege as string</returns>
+    public string LogIn(string? userName, string? password)
     {
-        if (userName.Trim().Equals("")) return false;
-        if (password.Trim().Equals("")) return false;
-
+        if (userName == null) return "Write your user name!";
+        if (password == null) return "Write your password!";
+        if (userName.Trim().Equals("")) return "User name is empty!";
+        if (password.Trim().Equals("")) return "Password is empty!";
 
         foreach (IUser user in Users)
         {
-            if (user.UserName == userName && user.Password == password)
-            {
-                SignedInUser = user;
-                return true;
-            }
+            if (!user.UserName.Equals(userName) || !user.Password.Equals(password)) continue;
+            SignedInUser = user;
+            return string.Empty;
+
         }
-        return false;
+        return "User name or password is wrong!";
     }
 
 
