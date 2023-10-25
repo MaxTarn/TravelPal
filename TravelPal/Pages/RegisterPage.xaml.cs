9267e6b9f5e938e3416145f0c6b8dc1fab505c28
MaxTarn <Maxtharn@gmail.com>
2023-10-26 01:37:51 +0200

    added methods that change what is in the combobox when the checkbox is changed. added a BtnRegister_click method (does not create a new user yet

--- TravelPal/Pages/RegisterPage.xaml.cs
+++ TravelPal/Pages/RegisterPage.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.ObjectiveC;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TravelPal.Classes;
 
 namespace TravelPal.Pages;
 
@@ -20,14 +25,40 @@ namespace TravelPal.Pages;
 /// </summary>
 public partial class RegisterPage : Page
 {
-    public string UserName { get; set; }
-    public string Password { get; set; }
-    public AllCountries ChosenCountry { get; set; }
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+    public Country? ChosenCountry { get; set; }
 
     public RegisterPage()
     {
         InitializeComponent();
-        var collectionOfAllEntries = Enum.GetValues(typeof(AllCountries));
+    }
+
+    private void ChkBxEuCountry_Checked(object sender, RoutedEventArgs e)
+    {
+        ComboBxCountries.Items.Clear();
+        foreach (Country country in Country.EUCountries)
+        {
+            ComboBoxItem listEntry = new ComboBoxItem { Content = country.ToString(), Tag = country };
+            ComboBxCountries.Items.Add(listEntry);
+        }
+    }
+
+    private void ChkBxEuCountry_OnUnchecked(object sender, RoutedEventArgs e)
+    {
+        ComboBxCountries.Items.Clear();
+        foreach (Country country in Country.NonEUCountries)
+        {
+
+            ComboBoxItem listEntry = new ComboBoxItem { Content = country.ToString(), Tag = country };
+            ComboBxCountries.Items.Add(listEntry);
+        }
+    }
+
+    private void BtnRegister_Click(object sender, RoutedEventArgs e)
+    {
+        ComboBoxItem selectedItem = ComboBxCountries.SelectedItem as ComboBoxItem;
+        MessageBox.Show(((Country)selectedItem.Tag).ToString());
     }
 }
 
