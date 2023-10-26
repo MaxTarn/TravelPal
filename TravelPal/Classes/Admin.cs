00ef96914f0a35a8ca66d7ce44e29f5f907e2651
MaxTarn <Maxtharn@gmail.com>
2023-10-27 00:55:49 +0200

    added a List<Travel>, added methods for adding to the list and for removeing from the list

--- TravelPal/Classes/Admin.cs
+++ TravelPal/Classes/Admin.cs
@@ -14,6 +14,16 @@ public class Admin : IUser
     public string UserName { get; set; }
     public string Password { get; set; }
     public Country? Location { get; set; }
+    public List<Travel> Travels { get; }
+    public void AddTravel(Travel travel)
+    {
+        Travels.Add(travel);
+    }
+
+    public void RemoveTravel(Travel travel)
+    {
+        Travels.Remove(travel);
+    }
 
 
     public Admin(string username, string password, Country location)
