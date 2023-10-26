5b68144b341077d81145a2809b790ef19acbf7a9
MaxTarn <Maxtharn@gmail.com>
2023-10-27 01:08:52 +0200

    added ValidUserInput(), added various event methods

--- TravelPal/Pages/RegisterPage.xaml.cs
+++ TravelPal/Pages/RegisterPage.xaml.cs
@@ -29,9 +29,15 @@ public partial class RegisterPage : Page
     public string? Password { get; set; }
     public Country? ChosenCountry { get; set; }
 
+    private SolidColorBrush? _btnLogInForeground;
+    private SolidColorBrush? _btnLogInBorderColor;
+
     public RegisterPage()
     {
         InitializeComponent();
+        ChkBxEuCountry_OnUnchecked(null, null); //so that the Combobox is not empty when page is first showed
+        _btnLogInBorderColor = BtnLogIn.BorderBrush as SolidColorBrush;
+        _btnLogInForeground = BtnLogIn.Foreground as SolidColorBrush;
     }
 
     private void ChkBxEuCountry_Checked(object sender, RoutedEventArgs e)
@@ -57,8 +63,62 @@ public partial class RegisterPage : Page
 
     private void BtnRegister_Click(object sender, RoutedEventArgs e)
     {
-        ComboBoxItem selectedItem = ComboBxCountries.SelectedItem as ComboBoxItem;
-        MessageBox.Show(((Country)selectedItem.Tag).ToString());
+        if (!ValidUserInput()) return;
+
+        LblErrorMessege.Content = Manager.UserManager.AddUser(new User(UserName, Password, ChosenCountry));
+        Manager.UserManager.LogIn(new User(UserName, Password, ChosenCountry));
+        Manager.HeaderPage.UpdateGUI();
+
+    }
+
+    private bool ValidUserInput()
+    {
+        //TODO add more if statements that describe why the user can not create a new account
+        //TODO add if statement that says that the length of the username is too short or too long
+        //TODO add if statement that says that the length of the password is too short or too long
+        if (UserName?.Trim().Length <= 3 || UserName.Equals(null))
+        {
+            LblErrorMessege.Content = "Write User Name!";
+            return false;
+        }
+
+        if (Password?.Trim().Length <= 3 || Password.Equals(null))
+        {
+            LblErrorMessege.Content = "Write Password!";
+            return false;
+        }
+
+        if (ComboBxCountries.SelectedItem == null)
+        {
+            LblErrorMessege.Content = "Chose your country of origin!";
+            return false;
+        }
+
+        return true;
+    }
+
+    private void ComboBxCountries_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        ChosenCountry = ((ComboBxCountries.SelectedItem as ComboBoxItem)?.Tag) as Country;
+    }
+
+    private void BtnLogIn_OnClick(object sender, RoutedEventArgs e)
+    {
+
+        Manager.Window.FrameMain.Content = Manager.LogInPage;
+    }
+
+    private void BtnLogIn_OnMouseEnter(object sender, MouseEventArgs e)
+    {
+        BtnLogIn.Foreground = new SolidColorBrush(Colors.Blue);
+        BtnLogIn.BorderBrush = new SolidColorBrush(Colors.Blue);
+
+    }
+
+    private void BtnLogIn_OnMouseLeave(object sender, MouseEventArgs e)
+    {
+        BtnLogIn.Foreground = _btnLogInForeground;
+        BtnLogIn.BorderBrush = _btnLogInBorderColor;
     }
 }
 
