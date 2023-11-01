574ba21e4ce985a31ea6625528f153a3b7787709
MaxTarn <Maxtharn@gmail.com>
2023-11-01 01:23:36 +0100

    added framework for making twoway databinding

--- TravelPal/Pages/TravelPage.xaml.cs
+++ TravelPal/Pages/TravelPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Runtime.InteropServices.ObjectiveC;
 using System.Text;
@@ -13,40 +14,65 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TravelPal.Classes;
 
-namespace TravelPal.Pages
+namespace TravelPal.Pages;
+
+/// <summary>
+/// Interaction logic for TravelPage.xaml
+/// </summary>
+public partial class TravelPage : Page, INotifyPropertyChanged
 {
-    /// <summary>
-    /// Interaction logic for TravelPage.xaml
-    /// </summary>
-    public partial class TravelPage : Page
+    //---------- Method and property for making databinding TwoWay and update in GUI----------
+
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged(string propertyName)
     {
-        public bool Testr { get; set; } = false;
-        public TravelPage()
-        {
-            InitializeComponent();
-        }
-
-        public void UpdateGUI()
-        {
-            ComboBxTravels.Items.Clear();
-            if (Manager.UserManager.SignedInUser?.Travels.Count <= 0)
-            {
-                ComboBoxItem defaultEntry = new ComboBoxItem();
-                defaultEntry.Content = "No Travels found.";
-                ComboBxTravels.Items.Add(defaultEntry);
-                ComboBxTravels.SelectedIndex = 0;
-                ComboBxTravels.IsEditable = false;
-                return;
-            }
-        }
-
-        private void BtnCreateNewTravel_Click(object sender, RoutedEventArgs e)
-        {
-            FrameMain.Content = Manager.AddNewTravelPage;
-        }
-
-
-
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+
+
+    //---------- Method and property for making databinding TwoWay and update in GUI END----------
+
+
+
+
+    public bool Testr { get; set; } = false;
+    public TravelPage()
+    {
+        InitializeComponent();
+    }
+
+
+
+    public void UpdateGUI()
+    {
+
+        ComboBxTravels.Items.Clear();
+        if (Manager.UserManager.SignedInUser?.Travels.Count <= 0)
+        {
+            ComboBoxItem defaultEntry = new ComboBoxItem();
+            defaultEntry.Content = "No Travels found.";
+            defaultEntry.Tag = null;
+            ComboBxTravels.Items.Add(defaultEntry);
+            return;
+        }
+        foreach (Travel travl in Manager.UserManager.SignedInUser.Travels)
+        {
+            ComboBoxItem containerForTravel = new();
+            containerForTravel.Tag = travl;
+            containerForTravel.Content = travl.TravelName;
+
+            ComboBxTravels.Items.Add(containerForTravel);
+        }
+    }
+
+    private void BtnCreateNewTravel_Click(object sender, RoutedEventArgs e)
+    {
+        FrameMain.Content = Manager.AddNewTravelPage;
+    }
+
+
 }
+
