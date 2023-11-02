0b71f0c203e32664122b8dbcf01219fcb2a018da
MaxTarn <Maxtharn@gmail.com>
2023-11-02 13:34:34 +0100

    added the InterFace required for twoway binding aswell as a colleciton for the Travels

--- TravelPal/Pages/ViewTravelPage.xaml.cs
+++ TravelPal/Pages/ViewTravelPage.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +15,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TravelPal.Classes;
 
-namespace TravelPal.Pages
+namespace TravelPal.Pages;
+
+/// <summary>
+/// Interaction logic for ViewTravelPage.xaml
+/// </summary>
+public partial class ViewTravelPage : Page, INotifyPropertyChanged
 {
-    /// <summary>
-    /// Interaction logic for ViewTravelPage.xaml
-    /// </summary>
-    public partial class ViewTravelPage : Page
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
-        public ViewTravelPage()
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private ObservableCollection<Travel> _travels;
+
+    public ObservableCollection<Travel> Travels
+    {
+        get => _travels;
+        set
         {
-            InitializeComponent();
+            if (_travels == value) return;
+            _travels = value;
+            OnPropertyChanged(nameof(Travels));
         }
     }
+
+
+
+
+    public ViewTravelPage()
+    {
+        InitializeComponent();
+    }
+
+
 }
+
