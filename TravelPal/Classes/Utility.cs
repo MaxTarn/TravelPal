dc2c801d66c375a8efec19327ebf3fea7215742f
MaxTarn <Maxtharn@gmail.com>
2023-11-02 13:26:09 +0100

    added TruncateOrPadIntTo(), aswell as TruncateOrPadStringTo() now returns a string of parameter: maximumLength of whitespaces when the inputString is null

--- TravelPal/Classes/Utility.cs
+++ TravelPal/Classes/Utility.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace TravelPal.Classes;
 
@@ -15,9 +16,9 @@ public static class Utility
     /// <param name="inputString"></param>
     /// <param name="maximumLength"></param>
     /// <returns></returns>
-    public static string TruncateOrPadTo(string? inputString, int maximumLength)
+    public static string TruncateOrPadStringTo(string? inputString, int maximumLength)
     {
-        if (inputString == null) return "";
+        if (inputString == null) return new string(' ', maximumLength);
         if (maximumLength <= 0) return "";
 
         inputString = inputString.Trim();
@@ -33,6 +34,21 @@ public static class Utility
         return outPutString;
 
 
+    }
+
+    public static string TruncateOrPadIntTo(int? inputNumber)
+    {
+        if (inputNumber == null) return "";
+        string outPutString;
+
+        int lengthOfInputNumber = inputNumber.ToString().Length;
+
+        if (inputNumber > 999) outPutString = ">999";
+        else if (lengthOfInputNumber == 1) outPutString = "   " + inputNumber.ToString();
+        else if (lengthOfInputNumber == 2) outPutString = "  " + inputNumber.ToString();
+        else outPutString = " " + inputNumber.ToString();
+
+        return outPutString;
     }
 }
 
