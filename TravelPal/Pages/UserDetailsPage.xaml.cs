fed0aa3aeb63223ea6915cc3e0748bfcb4f9b57c
MaxTarn <Maxtharn@gmail.com>
2023-11-02 22:50:20 +0100

    allows the user to view the chosen Travel, chosen on ChooseTravelPage (not working atm)

--- TravelPal/Pages/UserDetailsPage.xaml.cs
+++ TravelPal/Pages/UserDetailsPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.Xml;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -34,20 +35,44 @@ public partial class UserDetailsPage : Page, INotifyPropertyChanged
 
 
 
+    private string? _userName;
+
+    public string UserName
+    {
+        get => _userName;
+        set
+        {
+            if (value == _userName) return;
+            _userName = value;
+            OnPropertyChanged();
+        }
+    }
+
     private ComboBoxItem? _selectedCountryOfOrigin;
     public ComboBoxItem? SelectedCountryOfOrigin
     {
         get => _selectedCountryOfOrigin;
         set
         {
-            if (_selectedCountryOfOrigin != value) return;
+            if (_selectedCountryOfOrigin == value) return;
             _selectedCountryOfOrigin = value;
             OnPropertyChanged();
         }
     }
 
 
-    public ObservableCollection<ComboBoxItem> Countries;
+    private ObservableCollection<ComboBoxItem>? _countries;
+    public ObservableCollection<ComboBoxItem>? Countries
+    {
+        get => _countries;
+        set
+        {
+            if (_countries == value) return;
+            _countries = value;
+            OnPropertyChanged();
+        }
+    }
+
 
     private bool _isEUCountry;
     public bool IsEUCountry
@@ -55,11 +80,60 @@ public partial class UserDetailsPage : Page, INotifyPropertyChanged
         get => _isEUCountry;
         set
         {
-            if (value != _isEUCountry) return;
+            if (value == _isEUCountry) return;
             _isEUCountry = value;
+            OnPropertyChanged();
+            ChangeCountriesInComboBox(value);
+        }
+    }
+
+
+
+    private string? _password;
+    public string? Password
+    {
+        get => _password;
+        set
+        {
+            if (_password == value) return;
+            _password = value;
             OnPropertyChanged();
         }
     }
+
+
+
+    private string? _repeatPassword;
+    public string? ReapeatPassword
+    {
+        get => _repeatPassword;
+        set
+        {
+            if (_repeatPassword == value) return;
+            _repeatPassword = value;
+            OnPropertyChanged();
+
+        }
+    }
+
+
+
+    private bool _inputIsEnabled;
+
+    public bool InputIsEnabled
+    {
+        get => _inputIsEnabled;
+        set
+        {
+            _inputIsEnabled = value;
+            OnPropertyChanged();
+            if (value) EnableInputFromUser();
+            if (!value) DisableInputFromUser();
+
+        }
+    }
+
+
 
 
 
@@ -68,10 +142,28 @@ public partial class UserDetailsPage : Page, INotifyPropertyChanged
     {
         InitializeComponent();
         IsEUCountry = false;
+        Countries = new();
         ChangeCountriesInComboBox(IsEUCountry);
+        InputIsEnabled = false;
+        MessageBox.Show(InputIsEnabled.ToString());
 
     }
 
+    private void EnableInputFromUser()
+    {
+        TxtBxUserName.IsEnabled = true;
+        ComboBxCountries.IsEnabled = true;
+        TxtBxPassword.IsEnabled = true;
+        TxtBxRepeatPassword.IsEnabled = true;
+    }
+
+    private void DisableInputFromUser()
+    {
+        TxtBxUserName.IsEnabled = false;
+        ComboBxCountries.IsEnabled = false;
+        TxtBxPassword.IsEnabled = false;
+        TxtBxRepeatPassword.IsEnabled = false;
+    }
 
 
     private void ChangeCountriesInComboBox(bool isEuCountry)
@@ -86,28 +178,28 @@ public partial class UserDetailsPage : Page, INotifyPropertyChanged
     }
     public void AddEUCountriesToComboBox()
     {
-        ComboBxCountries.Items.Clear();
+        Countries.Clear();
 
         Country.EUCountries.ForEach(country =>
         {
             ComboBoxItem container = new();
             container.Tag = country;
             container.Content = country.ToString();
-            ComboBxCountries.Items.Add(container);
+            Countries.Add(container);
         });
 
     }
 
     public void AddNonEUCountriesToComboBox()
     {
-        ComboBxCountries.Items.Clear();
+        Countries.Clear();
 
         Country.NonEUCountries.ForEach(country =>
         {
             ComboBoxItem container = new();
             container.Tag = country;
             container.Content = country.ToString();
-            ComboBxCountries.Items.Add(container);
+            Countries.Add(container);
         });
     }
 
@@ -126,6 +218,61 @@ public partial class UserDetailsPage : Page, INotifyPropertyChanged
                 SelectedCountryOfOrigin = container;
             }
         }
+
+        UserName = Manager.UserManager.SignedInUser.UserName;
+        Password = Manager.UserManager.SignedInUser.Password;
+    }
+
+    private void BtnEditDetails_Click(object sender, RoutedEventArgs e)
+    {
+        InputIsEnabled = !InputIsEnabled;
+    }
+
+    private void BtnSaveDetails_Click(object sender, RoutedEventArgs e)
+    {
+        string? errorMessege = ValidateInputs();
+        if (errorMessege != null)
+        {
+            MessageBox.Show(errorMessege);
+            return;
+        }
+
+        if (Manager.UserManager.SignedInUser == null)
+        {
+            MessageBox.Show("ERROR: Not logged in");
+            Manager.Window.FrameMain.Content = Manager.LogInPage;
+            return;
+        }
+
+        Manager.UserManager.SignedInUser.UserName = UserName;
+        Manager.UserManager.SignedInUser.CountryOfOrigin = SelectedCountryOfOrigin.Tag as Country;
+        Manager.UserManager.SignedInUser.Password = Password;
+
+        MessageBox.Show("Succses");
+        Manager.HeaderPage.UpdateGUI();
+
+    }
+
+    public string? ValidateInputs()
+    {
+        if (string.IsNullOrEmpty(UserName)) return "User name is too short!";
+        if (SelectedCountryOfOrigin == null) return "Choose a country of origin";
+        if (Password != ReapeatPassword) return "The passwords are not the same!";
+        if (string.IsNullOrEmpty(Password)) return "Password is empty";
+
+        if (UserName.Length <= 3) return "User name is too short";
+        if (Password.Length <= 3) return "Password is too short";
+
+
+        return null;
+    }
+
+    private void ClearFields()
+    {
+        UserName = "";
+        SelectedCountryOfOrigin = null;
+        Password = "";
+        ReapeatPassword = "";
     }
 }
 
