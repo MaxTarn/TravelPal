6b639d6932e840c4565c3178799ccb6f8aa58f0d
MaxTarn <Maxtharn@gmail.com>
2023-11-02 22:43:21 +0100

    implements IPropertyChanged or whateeve its called

--- TravelPal/Classes/Admin.cs
+++ TravelPal/Classes/Admin.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using TravelPal.Interfaces;
@@ -11,10 +13,17 @@ namespace TravelPal.Classes;
 
 public class Admin : IUser
 {
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
     public string UserName { get; set; }
     public string Password { get; set; }
-    public Country? CountryOfOrigin { get; set; }
-    public List<Travel> Travels { get; } = new();
+    public Country CountryOfOrigin { get; set; }
+    public List<Travel> Travels { get; set; }
     public void AddTravel(Travel travel)
     {
         Travels.Add(travel);
@@ -32,5 +41,7 @@ public class Admin : IUser
         Password = password;
         CountryOfOrigin = location;
     }
+
+
 }
 
